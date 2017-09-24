73b39579f02a97ef56d58618a82c048ae7b97ca3
KH <kasperkh.kh@gmail.com>
2017-09-24 21:09:32 +0200

    Sliders show their background color on hover, and keeps it when you click-and-hold it

--- web/src/static/global.js
+++ web/src/static/global.js
@@ -1099,6 +1099,7 @@ function updateColVisibility() {
     };
 
     var progressSlider = document.querySelector(".player .progress-slider");
+    var progressSliderElement;
     var progressMouseDown = false;
     rangeSlider.create(progressSlider, {
         rangeClass: "rangeSlider progress-slider",
@@ -1107,12 +1108,17 @@ function updateColVisibility() {
         max: 1,
         step: 0.0002,
         value: 0,
+        onInit: function() {
+            progressSliderElement = document.querySelector(".player .rangeSlider.progress-slider")
+        },
         onSlideStart: function() {
             progressMouseDown = true;
+            progressSliderElement.classList.add("mouse-down");
         },
         onSlideEnd: function(value) {
             progressMouseDown = false;
             audio.currentTime = value * audio.duration;
+            progressSliderElement.classList.remove("mouse-down");
         }
     });
     function updateWhilePlaying() {
@@ -1213,6 +1219,7 @@ function updateColVisibility() {
     if (volume) volume = Number(volume);
     else volume = 1;
     audio.volume = volume;
+    var volumeContainer = document.querySelector(".player .volume-container");
     rangeSlider.create(volumeSlider, {
         rangeClass: "rangeSlider volume-slider",
         polyfill: true,
@@ -1225,13 +1232,34 @@ function updateColVisibility() {
         },
         onSlide: function(value) {
             audio.volume = value;
+            updateVolIcon(value);
+            volumeContainer.classList.add("mouse-down");
         },
         onSlideEnd: function(value) {
             volumeMouseDown = false;
             audio.volume = value;
             localStorage.setItem("volume", String(value));
+            updateVolIcon(value);
+            volumeContainer.classList.remove("mouse-down");
         }
     });
+
+    var volIcon = document.querySelector(".player svg.icon.volume");
+    updateVolIcon(audio.volume);
+    function updateVolIcon(value) {
+        if (value == 0 && volIcon.dataset.loudness != "0") {
+            volIcon.dataset.loudness = "0";
+            volIcon.innerHTML = '<path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"></path><path d="M0 0h24v24H0z" fill="none"></path>';
+        }
+        if (value < 0.5 && volIcon.dataset.loudness != "2") {
+            volIcon.dataset.loudness = "2";
+            volIcon.innerHTML = '<path d="M18.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM5 9v6h4l5 5V4L9 9H5z"></path><path d="M0 0h24v24H0z" fill="none"></path>';
+        }
+        if (value > 0.5 && volIcon.dataset.loudness != "3") {
+            volIcon.dataset.loudness = "3";
+            volIcon.innerHTML = '<path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"></path><path d="M0 0h24v24H0z" fill="none"></path>';
+        }
+    }
 })();
 
 // HOME PAGE
