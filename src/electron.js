47bd8a9e879bdff4c1fbeb01d37dcc9a7cd34c33
Kasper <kasperkh.kh@gmail.com>
2020-12-21 02:19:13 +0100

    Fix CSP

--- src/electron.js
+++ src/electron.js
@@ -1,10 +1,9 @@
-const { app, globalShortcut, ipcMain, BrowserWindow, Electron, Menu } = require('electron')
+const { app, session, globalShortcut, ipcMain, BrowserWindow, Electron, Menu } = require('electron')
 const path = require('path')
 
 let mainWindow
 const dev = process.env.APP_ENV === 'dev'
 const devPort = process.env.DEV_PORT
-console.log('::::procenv', process.env)
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -19,16 +18,26 @@ function createWindow() {
       contextIsolation: true,
       nodeIntegration: true,
     },
-    // webPreferences: {
-    //   nodeIntegration: true,
-    // },
     show: false,
   })
-  if (dev) {
-    mainWindow.loadURL('http://localhost:'+devPort)
-  } else {
-    mainWindow.loadFile(path.resolve(__dirname, '../public/index.html'))
-  }
+
+  // Here we set the CSP to allow 'unsafe-eval' for 'self' because Nollup
+  // uses eval(). This causes a security warning, so we disable that. This
+  // should be fine because 'self' won't be an external origin, as we only
+  // index.html locally.
+  mainWindow.ELECTRON_DISABLE_SECURITY_WARNINGS = true
+  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    callback({
+      responseHeaders: {
+        ...details.responseHeaders,
+        'Content-Security-Policy': ["script-src 'self' 'unsafe-inline' 'unsafe-eval';"],
+      },
+    })
+  })
+
+  if (dev) mainWindow.loadURL('http://localhost:'+devPort)
+  else mainWindow.loadFile(path.resolve(__dirname, '../public/index.html'))
+
   mainWindow.once('ready-to-show', () => {
     mainWindow.show()
   })
