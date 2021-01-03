46523bc1fdc24a858a1710e37d8d6ce71bbf4a22
Kasper <kasperkh.kh@gmail.com>
2021-01-03 19:59:43 +0100

    Fixed app name being "Ferrum Dev" in production

--- src/scripts/paths.js
+++ src/scripts/paths.js
@@ -9,8 +9,6 @@ else throw new Error('No electron.app found')
 const appData = app.getPath('appData')
 const musicPath = app.getPath('music')
 
-app.setName('Ferrum Dev')
-
 const electronData = path.join(appData, app.name, 'Electron Data')
 const libraryPath = path.join(musicPath, app.name)
 const libraryJsonPath = path.join(libraryPath, 'library.json')
