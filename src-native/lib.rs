d33198c94ffd78d55bc8df4df9bcf81d77ec5ff9
Kasper <kasperkh.kh@gmail.com>
2024-10-07 12:27:30 +0200

    Remove unused UniResult

--- src-native/lib.rs
+++ src-native/lib.rs
@@ -2,7 +2,6 @@ use anyhow::{Context, Result};
 use serde::de::DeserializeOwned;
 use std::fs::File;
 use std::io::BufReader;
-use std::mem;
 use std::path::PathBuf;
 use std::time::{SystemTime, UNIX_EPOCH};
 
@@ -42,35 +41,6 @@ fn str_to_option(s: String) -> Option<String> {
 	match s.as_str() {
 		"" => None,
 		_ => Some(s),
-	}
-}
-
-pub type UniResult<T> = std::result::Result<T, UniError>;
-
-pub struct UniError {
-	pub message: String,
-}
-impl From<String> for UniError {
-	fn from(message: String) -> Self {
-		Self { message }
-	}
-}
-impl From<&str> for UniError {
-	fn from(message: &str) -> Self {
-		Self {
-			message: message.to_string(),
-		}
-	}
-}
-impl From<UniError> for napi::Error {
-	fn from(ue: UniError) -> Self {
-		Self::from_reason(ue.message)
-	}
-}
-impl From<napi::Error> for UniError {
-	fn from(mut n_err: napi::Error) -> Self {
-		let message = mem::replace(&mut n_err.reason, "".to_string());
-		UniError { message }
 	}
 }
 
