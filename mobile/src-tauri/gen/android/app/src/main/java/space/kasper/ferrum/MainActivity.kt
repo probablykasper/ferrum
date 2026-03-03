058dbae06533babac304be6a1640cb9425ff5a0c
Kasper <kasperkh.kh@gmail.com>
2026-03-03 22:33:16 +0100

    Init Tauri mobile app

---
+++ mobile/src-tauri/gen/android/app/src/main/java/space/kasper/ferrum/MainActivity.kt
@@ -0,0 +1,11 @@
+package space.kasper.ferrum
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
